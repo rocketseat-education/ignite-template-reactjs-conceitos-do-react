269bdcc807de5f907801e5f704f1031d1a5a409b
Giovanne Tarcitano <44866256+Gitarcitano@users.noreply.github.com>
2021-03-16 22:45:29 -0300

    remove unused imports

--- src/__tests__/components/TaskList.spec.tsx
+++ src/__tests__/components/TaskList.spec.tsx
@@ -1,5 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { TaskList } from '../../components/TaskList';
 
 describe('App Page', () => {
